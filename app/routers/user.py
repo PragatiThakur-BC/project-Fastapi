159adb8af6ed3a28728075041c4cbf041b45394a
Pragati Thakur <pragati.thakur@blenheimchalcot.com>
2023-11-03 15:48:48 +0530

    updating routers

---
+++ app/routers/user.py
@@ -0,0 +1,26 @@
+from .. import models, schema, services
+from fastapi import APIRouter ,FastAPI, status, HTTPException, Response, Depends
+from sqlalchemy.orm import Session
+from ..database import get_db
+
+router = APIRouter()
+
+
+@router.post("/users", status_code=status.HTTP_201_CREATED, response_model=schema.UserOut)
+def create_user(user: schema.UserCreate, db: Session = Depends(get_db)):
+    # hashing the password to store it in DB - user.password
+    user.password = services.hash_password(user.password)  # now user.password has hashed password
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@router.get("/users/{id}", response_model=schema.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"user with {id} does not exist")
+    return user
